e50c3d03253ad800038f1f826e69cb33adc077e7
Meet Patel <pate1867@purdue.edu>
2025-04-03 10:18:58 -0400

    commit 1

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,91 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+  struct list_node *node = malloc(sizeof(struct list_node));
+    if (node == NULL) {
+      return NULL;
+    }
+    node->value = value;
+    node->next = NULL;
+    return node;
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *node = new_node(value);
+    if (node == NULL) {
+      return;
+    }
+    node->next = list->head;
+    list->head = node;
+    if (list->tail == NULL) {
+      list->tail = node;
+    }
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node *node = new_node(value);
+    if (node == NULL) {
+      return;
+    }
+    if (list->tail == NULL) {
+      list->head = node;
+      list->tail = node;
+    } else {
+      list->tail->next = node;
+      list->tail = node;
+    }
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) { 
+  if (list->head == NULL) {
+      return 0; // or some error value
+    }
+    struct list_node *node = list->head;
+    size_t value = node->value;
+    list->head = node->next;
+    if (list->head == NULL) {
+      list->tail = NULL;
+    }
+    free(node);
+    return value;
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list) { 
+  if (list->tail == NULL) {
+    return 0; // or some error value
+  }
+    
+  // If there's only one node
+  if (list->head == list->tail) {
+    size_t value = list->head->value;
+    free(list->head);
+    list->head = NULL;
+    list->tail = NULL;
+    return value;
+  }
+    
+  // Find the node before tail
+  struct list_node *prev = list->head;
+  while (prev->next != list->tail) {
+    prev = prev->next;
+  }
+    
+  size_t value = list->tail->value;
+  free(list->tail);
+  list->tail = prev;
+  list->tail->next = NULL;
+  return value;
+}
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list) {
+  struct list_node *current = list.head;
+    while (current != NULL) {
+      struct list_node *next = current->next;
+      free(current);
+      current = next;
+    }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,76 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+    size_t serialized = serialize(state);
+    insert_at_tail(&q->list, serialized);
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) {
+    size_t serialized = remove_from_head(&q->list);
+    return deserialize(serialized);
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) {
+    struct game_state goal = {
+        .board = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 0}
+        },
+        .empty_row = 3,
+        .empty_col = 3,
+        .moves = 0
+    };
+
+    if (memcmp(start.board, goal.board, sizeof(start.board)) == 0) {
+        return 0;
+    }
+
+    struct queue q = { .list = { .head = NULL, .tail = NULL } };
+    enqueue(&q, start);
+
+    #define MAX_VISITED 100000
+    size_t visited[MAX_VISITED];
+    size_t visited_count = 0;
+
+    while (q.list.head != NULL) {
+        struct game_state current = dequeue(&q);
+        int directions[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int i = 0; i < 4; i++) {
+            int new_row = current.empty_row + directions[i][0];
+            int new_col = current.empty_col + directions[i][1];
+            if (new_row >= 0 && new_row < 4 && new_col >= 0 && new_col < 4) {
+                // Create new state by swapping empty space with neighbor
+                struct game_state new_state = current;
+                new_state.board[current.empty_row][current.empty_col] = 
+                    new_state.board[new_row][new_col];
+                new_state.board[new_row][new_col] = 0;
+                new_state.empty_row = new_row;
+                new_state.empty_col = new_col;
+                new_state.moves++;
+                if (memcmp(new_state.board, goal.board, sizeof(goal.board)) == 0) {
+                    free_list(q.list);
+                    return new_state.moves;
+                }
+                size_t serialized = serialize(new_state);
+                int found = 0;
+                for (size_t j = 0; j < visited_count; j++) {
+                    if (visited[j] == serialized) {
+                        found = 1;
+                        break;
+                    }
+                }
+                if (!found && visited_count < MAX_VISITED) {
+                    enqueue(&q, new_state);
+                    visited[visited_count++] = serialized;
+                }
+            }
+        }
+    }
+
+    free_list(q.list);
+    return -1; 
+
+}
